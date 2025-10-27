f418bb89a26bd4455f5194d81b33e77ddc9105e1
Kavin <kavinssj2@gmail.com>
2025-10-27 22:52:38 +0530

    MainMenu WIP

---
+++ Raylib-Pallanguzhi/MainMenu.c
@@ -0,0 +1,20 @@
+#include "MainMenu.h"
+
+void InitializeMainMenu(MainMenu* mainMenu)
+{
+    
+}
+
+void DrawMainMenu(MainMenu* mainMenu)
+{
+}
+
+bool IsPlayButtonClicked(MainMenu* mainMenu)
+{
+    return false;
+}
+
+bool IsExitButtonClicked(MainMenu* mainMenu)
+{
+    return false;
+}
---
+++ Raylib-Pallanguzhi/MainMenu.h
@@ -0,0 +1,25 @@
+#include "raylib.h"
+
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+typedef struct
+{
+    Rectangle playButton;
+    Rectangle exitButton;
+} MainMenu;
+
+//need to draw BG first
+//need to draw two buttons, play and exit
+//have a update method, that checks for the button click on them
+
+//what do we need, the Textures for the BG and the buttons
+//coordinates for where to draw the buttons
+void InitializeMainMenu(MainMenu* mainMenu);
+
+void DrawMainMenu(MainMenu* mainMenu);
+
+bool IsPlayButtonClicked(MainMenu* mainMenu);
+
+bool IsExitButtonClicked(MainMenu* mainMenu);
+#endif
